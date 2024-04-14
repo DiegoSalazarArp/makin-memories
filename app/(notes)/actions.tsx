aeba27caee2c247dd195331ce65ef04f3d747d15
DiegoSalazar <d.salazararp@gmail.com>
2024-04-14 11:21:44 -0400

    Corregidos estilos en la clase main de page.tsx y añadida funcionalidad de eliminar notas en actions.tsx

--- app/(notes)/actions.tsx
+++ app/(notes)/actions.tsx
@@ -2,6 +2,7 @@
 
 import { CreateNoteSchema } from "@/lib/schema"
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs"
+import { revalidatePath } from "next/cache"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
@@ -58,4 +59,31 @@ export async function createNoteActions(prevState: any, formData: FormData) {
       error: e.toString(),
     }
   }
+}
+
+export async function deleteNoteActions(formData: FormData) {
+  const id = formData.get("id")
+
+  try {
+    const supabase = createServerActionClient({ cookies })
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
+
+    if (user === null) return
+
+    const result = await supabase.from("todos").delete().eq("id", id)
+
+    console.log(result)
+
+    revalidatePath("/dashboard/list")
+  } catch (e: any) {
+    return {
+      ok: null,
+      error: e.toString(),
+    }
+  }
+
+  try {
+  } catch (e: any) {}
 }
--- components/notes/MyList.tsx
+++ components/notes/MyList.tsx
@@ -26,9 +26,12 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import { cookies } from "next/headers"
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { Suspense } from "react"
-import { SkeletonList } from "../Skeleton"
+import {
+  createClientComponentClient,
+  createServerComponentClient,
+} from "@supabase/auth-helpers-nextjs"
+import SubmitButton from "../auth/submit-button"
+import { deleteNoteActions } from "@/app/(notes)/actions"
 
 export default async function MyList() {
   const supabase = createServerComponentClient({ cookies })
@@ -46,90 +49,96 @@ export default async function MyList() {
     return date.toLocaleDateString("en-CA") // 'en-CA' usa el formato yyyy-MM-dd
   }
 
-  console.log(todos)
-
   return (
-    <Suspense fallback={<SkeletonList />}>
-      <main className="animate-in flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-        <div className="flex items-center mb-10">
-          <h1 className="text-lg font-semibold md:text-2xl">
-            This is your list {":)"}
-          </h1>
-        </div>
-        <div>
-          <Card>
-            <CardHeader>
-              <CardTitle>Notes</CardTitle>
-              <CardDescription>Manage your notes.</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Subject</TableHead>
-                    <TableHead>Watch</TableHead>
-                    <TableHead>Email to</TableHead>
-                    <TableHead className="hidden md:table-cell">
-                      Dispatch date
-                    </TableHead>
+    // <Suspense fallback={<SkeletonList />}>
+    <main className="animate-in flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+      <div className="flex items-center mb-10">
+        <h1 className="text-lg font-semibold md:text-2xl">
+          This is your list {":)"}
+        </h1>
+      </div>
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Notes</CardTitle>
+            <CardDescription>Manage your notes.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Subject</TableHead>
+                  <TableHead>Watch</TableHead>
+                  <TableHead>Email to</TableHead>
+                  <TableHead className="hidden md:table-cell">
+                    Dispatch date
+                  </TableHead>
 
-                    <TableHead>
-                      <span className="sr-only">Actions</span>
-                    </TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {todos!.map((todo: any) => (
-                    <TableRow key={todo.id}>
-                      <TableCell className="font-medium line-clamp-1">
-                        <span className="truncate">{todo.subject}</span>
-                      </TableCell>
-                      <TableCell>
-                        <Badge variant="outline">
-                          {todo.seen ? (
-                            <Eye className="text-green-400" />
-                          ) : (
-                            <EyeOff className="text-red-400" />
-                          )}
-                        </Badge>
-                      </TableCell>
-                      <TableCell>{todo.email_to}</TableCell>
-                      <TableCell className="hidden md:table-cell">
-                        {formatDate(todo.dispatch_date)}
-                      </TableCell>
+                  <TableHead>
+                    <span className="sr-only">Actions</span>
+                  </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {todos!.map((todo: any) => (
+                  <TableRow key={todo.id}>
+                    <TableCell className="font-medium line-clamp-1">
+                      <span className="truncate">{todo.subject}</span>
+                    </TableCell>
+                    <TableCell>
+                      <Badge variant="outline">
+                        {todo.seen ? (
+                          <Eye className="text-green-400" />
+                        ) : (
+                          <EyeOff className="text-red-400" />
+                        )}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>{todo.email_to}</TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      {formatDate(todo.dispatch_date)}
+                    </TableCell>
 
-                      <TableCell>
-                        <DropdownMenu>
-                          <DropdownMenuTrigger asChild>
-                            <Button
-                              aria-haspopup="true"
-                              size="icon"
-                              variant="ghost"
-                            >
-                              <MoreHorizontal className="h-4 w-4" />
-                              <span className="sr-only">Toggle menu</span>
-                            </Button>
-                          </DropdownMenuTrigger>
-                          <DropdownMenuContent align="end">
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            aria-haspopup="true"
+                            size="icon"
+                            variant="ghost"
+                          >
+                            <MoreHorizontal className="h-4 w-4" />
+                            <span className="sr-only">Toggle menu</span>
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <form action={deleteNoteActions}>
+                            <input type="hidden" name="id" value={todo.id} />
+
                             <DropdownMenuLabel>Actions</DropdownMenuLabel>
                             <DropdownMenuItem>Edit</DropdownMenuItem>
-                            <DropdownMenuItem>Delete</DropdownMenuItem>
-                          </DropdownMenuContent>
-                        </DropdownMenu>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </CardContent>
-            <CardFooter>
-              <div className="text-xs text-muted-foreground">
-                Showing <strong>1-10</strong> of <strong>32</strong> products
-              </div>
-            </CardFooter>
-          </Card>
-        </div>
-      </main>
-    </Suspense>
+                            <DropdownMenuItem>
+                              <Button variant="ghost" size="sm">
+                                Eliminar
+                              </Button>
+                            </DropdownMenuItem>
+                          </form>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+          <CardFooter>
+            <div className="text-xs text-muted-foreground">
+              Showing <strong>1-10</strong> of <strong>32</strong> products
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </main>
+    // </Suspense>
   )
 }
